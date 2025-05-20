752c7888c71300cd3d3db7328bb28d2ecf79248e
alexander <alexander12110@gmail.com>
2025-05-20 16:45:55 +0700

    refactor: remove unused components and pages to streamline the application structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,58 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import IndexPage from '@/pages/IndexPage.vue';
-import InitDataPage from '@/pages/InitDataPage.vue';
-import ThemeParamsPage from '@/pages/ThemeParamsPage.vue';
-import LaunchParamsPage from '@/pages/LaunchParamsPage.vue';
-import TonConnectPage from '@/pages/TonConnectPage.vue';
-import IconTonConnect from '@/components/IconTonConnect.vue';
+
 import MainPage from '@/pages/MainPage.vue';
 
 export const routes = [
   {
-    path: '/main',
-    name: 'main',
-    component: MainPage,
-    meta:{
-    title: 'MAIN PAGE',
-    },
-  },
-  {
     path: '/',
     name: 'index',
-    component: IndexPage,
-  },
-  {
-    path: '/init-data',
-    name: 'init-data',
-    component: InitDataPage,
-    meta: {
-      title: 'Init Data',
-    },
-  },
-  {
-    path: '/theme-params',
-    name: 'theme-params',
-    component: ThemeParamsPage,
-    meta: {
-      title: 'Theme Params',
-    },
-  },
-  {
-    path: '/launch-params',
-    name: 'launch-params',
-    component: LaunchParamsPage,
-    meta: {
-      title: 'Launch Params',
-    },
-  },
-  {
-    path: '/ton-connect',
-    name: 'ton-connect',
-    component: TonConnectPage,
-    meta: {
-      icon: IconTonConnect,
-      title: 'TON Connect',
-    },
+    component: MainPage,
   }
 ];
 
